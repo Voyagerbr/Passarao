c3fd2c2f3957bfbb5f768db899ce76e4895f883e
Nicolas <slateste295@gmail.com>
2024-10-09 09:09:17 -0300

    Partial commit

---
+++ passarin/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace passarin;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ passarin/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace passarin;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+}
+
