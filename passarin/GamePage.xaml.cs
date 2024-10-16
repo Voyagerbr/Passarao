e3653cb39d12f8a01d27c2e6674280439deebef9
Nicolas <nicolasdealmeidasoares71@gmail.com>
2024-10-16 09:56:47 -0300

    COmmit dya 16/10/2024

--- passarin/GamePage.xaml.cs
+++ passarin/GamePage.xaml.cs
@@ -5,18 +5,19 @@ public partial class GamePage : ContentPage
 
 	double HeightWindow = 0;
 	double WidthtWindow = 0;
-	const int Velocity = 7;
-	const int Gravity = 3;
-	
+	const int Velocity = 12;
+	const int Gravity = 5;
+
 
 	const int TimeBeteweenFrames = 25;
 
 	bool IsDied = true;
 	const int JumpForce = 30;
-	const int maxJumpTime = 3;
+	const int maxJumpTime = 5;
 	bool IsJumping = false;
 	int JumpTime = 0;
-	
+	const int minOpening = 200;
+
 
 
 	// Ai
@@ -53,7 +54,7 @@ public partial class GamePage : ContentPage
 
 	bool VerifyColisionC()
 	{
-		var maxY = HeightWindow / 2 - Chao.HeightRequest - 37;
+		var maxY = HeightWindow / 2 - Chao.HeightRequest - 50;
 		if (Passaro.TranslationY >= maxY)
 		{
 			return true;
@@ -67,9 +68,9 @@ public partial class GamePage : ContentPage
 	void ApplyGravity()
 	{
 		Passaro.TranslationY += Gravity;
-		
+
 	}
-	ApplyJump()
+	void ApplyJump()
 	{
 		Passaro.TranslationY -= JumpForce;
 		JumpTime++;
@@ -82,13 +83,14 @@ public partial class GamePage : ContentPage
 
 	public async void Desenha()
 	{
+		Passaro.Source = "corvo.png";
 		while (!IsDied)
 		{
 			if (IsJumping)
 			{
 				ApplyJump();
 			}
-			else 
+			else
 			{
 				ApplyGravity();
 			}
@@ -97,6 +99,7 @@ public partial class GamePage : ContentPage
 			if (VerifyColision())
 			{
 				IsDied = true;
+				Passaro.Source = "kabum.png";
 				GameOverFrame.IsVisible = true;
 				break;
 			}
@@ -115,8 +118,13 @@ public partial class GamePage : ContentPage
 		PredioBaixo.TranslationX -= Velocity;
 		PredioCima.TranslationX -= Velocity;
 
+
 		if (PredioBaixo.TranslationX < -WidthtWindow)
 		{
+			var maxHeight = -100;
+			var minHeight = -PredioBaixo.HeightRequest;
+			PredioCima.TranslationY = Random.Shared.Next((int)maxHeight, (int)minHeight);
+			PredioBaixo.TranslationY = PredioCima.TranslationY + minOpening;//+ PredioBaixo.HeightRequest;
 			PredioBaixo.TranslationX = 0;
 			PredioCima.TranslationX = 0;
 		}
@@ -127,6 +135,7 @@ public partial class GamePage : ContentPage
 
 	void Ai(Object s, TappedEventArgs e)
 	{
+
 		GameOverFrame.IsVisible = false;
 		IsDied = false;
 		Inicializar();
@@ -136,9 +145,11 @@ public partial class GamePage : ContentPage
 	void Inicializar()
 	{
 		Passaro.TranslationY = 0;
+		PredioBaixo.TranslationX = 0;
+		PredioCima.TranslationX = 0;
 	}
 
-	void Ui (Object s, TappedEventArgs e)
+	void Ui(Object s, TappedEventArgs e)
 	{
 		IsJumping = true;
 	}
