1e37910985dd23b7b072f03b65e3a6731f25eac9
Nicolas <slateste295@gmail.com>
2024-10-29 08:55:01 -0300

    Sim

---
+++ passarin/SoundHelper.cs
@@ -0,0 +1,22 @@
+
+
+
+using Plugin.Maui.Audio;
+
+public class SoundHelper
+{
+  //------------------------------------------------------------------------
+
+  public static void Play(string nomeArquivoWav)
+  {
+    Task.Run(async () =>
+    {
+      var audioFX = await FileSystem.OpenAppPackageFileAsync(nomeArquivoWav);
+      var audioPlayer = AudioManager.Current.CreatePlayer(audioFX);
+      audioPlayer.Play();
+    });
+  }
+
+  //------------------------------------------------------------------------
+
+}
